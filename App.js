403094409daf0a5dab997a943562ad09108236e8
Rob <Robert_jhs@hotmail.com>
2022-06-28 09:54:54 -0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+  <NavigationContainer>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome this is the beginning of this</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
